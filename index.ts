dcbf5915f89fdfe138a942dd0b9bdb9d1ce19e67
chocoscoding <oyetit@gmail.com>
2022-05-18 00:08:13 +0100

    .

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import fastify from "fastify";
-const data = require("./data.json")
+// const data = require("./data.json")
+import * as data from "./data.json"
 
 const server = fastify();
 
@@ -7,7 +8,7 @@ server.get("/", async (request, reply) => {
   reply.code(200).send({ data});
 });
 
-server.listen(process.env.PORT || 8080, "0.0.0.0", (err, address) => {
+server.listen(process.env.PORT || 8080, "127.0.0.1", (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
